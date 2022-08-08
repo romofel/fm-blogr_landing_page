ecc17c8df4b730346b9363051cc3a3ddf3a5c6f5
Felipe Romo <felipe@romofel.dev>
2022-08-08 15:24:12 -0500

    Add features section

--- src/App.js
+++ src/App.js
@@ -54,7 +54,32 @@ function App() {
         </p>
       </section>
 
-      <section id="features"></section>
+      <section id="features">
+        <div>
+          <img src="" alt="" />
+        </div>
+        <div>
+          <div>
+            <h2>Free, open, simple</h2>
+            <p>
+              Blogr is a free and open source application backed by a large
+              community of helpful developers. It supports features such as code
+              syntax highlighting, RSS feeds, social media integration,
+              third-party commenting tools, and works seamlessly with Google
+              Analytics. The architecture is clean and is relatively easy to
+              learn.
+            </p>
+          </div>
+          <div>
+            <h2>Powerful tooling</h2>
+            <p>
+              Batteries included. We build a simple and straightforward CLI tool
+              that makes customization and deployment a breeze, but capable of
+              producing even the most complicated sites.
+            </p>
+          </div>
+        </div>
+      </section>
 
       <footer>
         <div>Blogr</div>
