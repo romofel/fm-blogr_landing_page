42be14a0d44dc340b33a7b622c10ce0f740e9018
Felipe Romo <felipe@romofel.dev>
2022-08-08 15:16:42 -0500

    Add html for the future section

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Hamburger from "hamburger-react";
 
@@ -19,6 +18,28 @@ function App() {
           <button>Learn More</button>
         </div>
       </div>
+
+      <section id="future">
+        <h1>Designed for the future</h1>
+        <div>
+          <img src="" alt="" />
+        </div>
+        <h2>Introducing an extensible editor</h2>
+        <p>
+          Blogr features an exceedingly intuitive interface which lets you focus
+          on one thing: creating content. The editor supports management of
+          multiple blogs and allows easy manipulation of embeds such as images,
+          videos, and Markdown. Extensibility with plugins and themes provide
+          easy ways to add functionality or change the looks of a blag.
+        </p>
+        <h2>Robust content management</h2>
+        <p>
+          Flexible content management enables users to easily move through
+          posts. Increase the usability of your blog by adding customized
+          categories, sections, format, or flow. With this functionality, you're
+          in full control.
+        </p>
+      </section>
     </div>
   );
 }
