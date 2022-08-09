f57c84be1ecdac7cddee993df7cd9ec6db996d71
Felipe Romo <felipe@romofel.dev>
2022-08-09 09:14:05 -0500

    Wrap the hero section with a landing bg div

--- src/App.js
+++ src/App.js
@@ -4,18 +4,21 @@ import Hamburger from "hamburger-react";
 function App() {
   return (
     <div>
-      <nav>
-        <div>Blogr</div>
-        <Hamburger />
-      </nav>
 
-      <div>
-        <h1>A modern publishing platform</h1>
-        <p>Grow your audience and build your online brand</p>
+      <div className="landing-bg">
+        <nav>
+          <div>Blogr</div>
+          <Hamburger />
+        </nav>
 
-        <div className="ctas">
-          <button>Start for Free</button>
-          <button>Learn More</button>
+        <div>
+          <h1>A modern publishing platform</h1>
+          <p>Grow your audience and build your online brand</p>
+
+          <div className="ctas">
+            <button>Start for Free</button>
+            <button>Learn More</button>
+          </div>
         </div>
       </div>
 
