cfd7426fd3d3dbfabf71a78c3546da73c3e6057d
Felipe Romo <felipe@romofel.dev>
2022-08-09 17:22:17 -0500

    Wrap an item in class

--- src/App.js
+++ src/App.js
@@ -102,14 +102,17 @@ function SectionFuture() {
         <img src={illustrationEditorMobile} alt="" />
       </div>
       <div className="future-content">
-        <h2>Introducing an extensible editor</h2>
-        <p>
-          Blogr features an exceedingly intuitive interface which lets you focus
-          on one thing: creating content. The editor supports management of
-          multiple blogs and allows easy manipulation of embeds such as images,
-          videos, and Markdown. Extensibility with plugins and themes provide
-          easy ways to add functionality or change the looks of a blag.
-        </p>
+        <div className="future-content-item">
+          <h2>Introducing an extensible editor</h2>
+          <p>
+            Blogr features an exceedingly intuitive interface which lets you
+            focus on one thing: creating content. The editor supports management
+            of multiple blogs and allows easy manipulation of embeds such as
+            images, videos, and Markdown. Extensibility with plugins and themes
+            provide easy ways to add functionality or change the looks of a
+            blag.
+          </p>
+        </div>
         <h2>Robust content management</h2>
         <p>
           Flexible content management enables users to easily move through
