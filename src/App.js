9f08deaea080e3a16cf79930f7e8a4fd5454514c
Felipe Romo <felipe@romofel.dev>
2022-08-09 17:05:46 -0500

    Refactor the html markup to its own components based on sections

--- src/App.js
+++ src/App.js
@@ -60,6 +60,117 @@ function HoverSubMenu({ items }) {
   );
 }
 
+function SectionFuture() {
+  return (
+    <section id="future">
+      <h1>Designed for the future</h1>
+      <div>
+        <img src="" alt="" />
+      </div>
+      <h2>Introducing an extensible editor</h2>
+      <p>
+        Blogr features an exceedingly intuitive interface which lets you focus
+        on one thing: creating content. The editor supports management of
+        multiple blogs and allows easy manipulation of embeds such as images,
+        videos, and Markdown. Extensibility with plugins and themes provide easy
+        ways to add functionality or change the looks of a blag.
+      </p>
+      <h2>Robust content management</h2>
+      <p>
+        Flexible content management enables users to easily move through posts.
+        Increase the usability of your blog by adding customized categories,
+        sections, format, or flow. With this functionality, you're in full
+        control.
+      </p>
+    </section>
+  );
+}
+
+function SectionState() {
+  return (
+    <section id="state-art">
+      <div>
+        <img src="" alt="" />
+      </div>
+      <h1>State of the Art Infrastructure</h1>
+      <p>
+        With reliability and speed in mind, worldwide data centers provide the
+        backbone for ultra-fast connectivity. This ensures your site will load
+        instantly, no matter where your readers are, keeping your site
+        competitive.
+      </p>
+    </section>
+  );
+}
+
+function SectionFeatures() {
+  return (
+    <section id="features">
+      <div>
+        <img src="" alt="" />
+      </div>
+      <div>
+        <div>
+          <h2>Free, open, simple</h2>
+          <p>
+            Blogr is a free and open source application backed by a large
+            community of helpful developers. It supports features such as code
+            syntax highlighting, RSS feeds, social media integration,
+            third-party commenting tools, and works seamlessly with Google
+            Analytics. The architecture is clean and is relatively easy to
+            learn.
+          </p>
+        </div>
+        <div>
+          <h2>Powerful tooling</h2>
+          <p>
+            Batteries included. We build a simple and straightforward CLI tool
+            that makes customization and deployment a breeze, but capable of
+            producing even the most complicated sites.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>
+      <div>Blogr</div>
+      <ul>
+        <li>
+          <span>Product</span>
+          <ul>
+            <li>Overview</li>
+            <li>Pricing</li>
+            <li>Marketplace</li>
+            <li>Features</li>
+            <li>Integrations</li>
+          </ul>
+        </li>
+        <li>
+          <span>Company</span>
+          <ul>
+            <li>About</li>
+            <li>Team</li>
+            <li>Blog</li>
+            <li>Careers</li>
+          </ul>
+        </li>
+        <li>
+          <span>Connect</span>
+          <ul>
+            <li>Contact</li>
+            <li>Newsletter</li>
+            <li>LinkedIn</li>
+          </ul>
+        </li>
+      </ul>
+    </footer>
+  );
+}
+
 function App() {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(true);
 
@@ -71,7 +182,10 @@ function App() {
             <div className="logo-container">
               <img src={logo} alt="company logo" />
             </div>
-            <Hamburger color="white" onToggle={toggled => setIsMobileMenuOpen(toggled)} />
+            <Hamburger
+              color="white"
+              onToggle={(toggled) => setIsMobileMenuOpen(toggled)}
+            />
           </div>
           {isMobileMenuOpen && <HoverMenu />}
         </nav>
@@ -89,100 +203,13 @@ function App() {
         </div>
       </div>
 
-      <section id="future">
-        <h1>Designed for the future</h1>
-        <div>
-          <img src="" alt="" />
-        </div>
-        <h2>Introducing an extensible editor</h2>
-        <p>
-          Blogr features an exceedingly intuitive interface which lets you focus
-          on one thing: creating content. The editor supports management of
-          multiple blogs and allows easy manipulation of embeds such as images,
-          videos, and Markdown. Extensibility with plugins and themes provide
-          easy ways to add functionality or change the looks of a blag.
-        </p>
-        <h2>Robust content management</h2>
-        <p>
-          Flexible content management enables users to easily move through
-          posts. Increase the usability of your blog by adding customized
-          categories, sections, format, or flow. With this functionality, you're
-          in full control.
-        </p>
-      </section>
+      <SectionFuture />
 
-      <section id="state-art">
-        <div>
-          <img src="" alt="" />
-        </div>
-        <h1>State of the Art Infrastructure</h1>
-        <p>
-          With reliability and speed in mind, worldwide data centers provide the
-          backbone for ultra-fast connectivity. This ensures your site will load
-          instantly, no matter where your readers are, keeping your site
-          competitive.
-        </p>
-      </section>
+      <SectionState />
 
-      <section id="features">
-        <div>
-          <img src="" alt="" />
-        </div>
-        <div>
-          <div>
-            <h2>Free, open, simple</h2>
-            <p>
-              Blogr is a free and open source application backed by a large
-              community of helpful developers. It supports features such as code
-              syntax highlighting, RSS feeds, social media integration,
-              third-party commenting tools, and works seamlessly with Google
-              Analytics. The architecture is clean and is relatively easy to
-              learn.
-            </p>
-          </div>
-          <div>
-            <h2>Powerful tooling</h2>
-            <p>
-              Batteries included. We build a simple and straightforward CLI tool
-              that makes customization and deployment a breeze, but capable of
-              producing even the most complicated sites.
-            </p>
-          </div>
-        </div>
-      </section>
+      <SectionFeatures />
 
-      <footer>
-        <div>Blogr</div>
-        <ul>
-          <li>
-            <span>Product</span>
-            <ul>
-              <li>Overview</li>
-              <li>Pricing</li>
-              <li>Marketplace</li>
-              <li>Features</li>
-              <li>Integrations</li>
-            </ul>
-          </li>
-          <li>
-            <span>Company</span>
-            <ul>
-              <li>About</li>
-              <li>Team</li>
-              <li>Blog</li>
-              <li>Careers</li>
-            </ul>
-          </li>
-          <li>
-            <span>Connect</span>
-            <ul>
-              <li>Contact</li>
-              <li>Newsletter</li>
-              <li>LinkedIn</li>
-            </ul>
-          </li>
-        </ul>
-      </footer>
+      <Footer />
     </div>
   );
 }
