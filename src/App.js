f9f519b6a7c3f7897d0165eec1910abfde961472
Felipe Romo <felipe@romofel.dev>
2022-08-09 17:20:53 -0500

    Wrap the content in a div

--- src/App.js
+++ src/App.js
@@ -101,21 +101,23 @@ function SectionFuture() {
       <div className="future-hero">
         <img src={illustrationEditorMobile} alt="" />
       </div>
-      <h2>Introducing an extensible editor</h2>
-      <p>
-        Blogr features an exceedingly intuitive interface which lets you focus
-        on one thing: creating content. The editor supports management of
-        multiple blogs and allows easy manipulation of embeds such as images,
-        videos, and Markdown. Extensibility with plugins and themes provide easy
-        ways to add functionality or change the looks of a blag.
-      </p>
-      <h2>Robust content management</h2>
-      <p>
-        Flexible content management enables users to easily move through posts.
-        Increase the usability of your blog by adding customized categories,
-        sections, format, or flow. With this functionality, you're in full
-        control.
-      </p>
+      <div className="future-content">
+        <h2>Introducing an extensible editor</h2>
+        <p>
+          Blogr features an exceedingly intuitive interface which lets you focus
+          on one thing: creating content. The editor supports management of
+          multiple blogs and allows easy manipulation of embeds such as images,
+          videos, and Markdown. Extensibility with plugins and themes provide
+          easy ways to add functionality or change the looks of a blag.
+        </p>
+        <h2>Robust content management</h2>
+        <p>
+          Flexible content management enables users to easily move through
+          posts. Increase the usability of your blog by adding customized
+          categories, sections, format, or flow. With this functionality, you're
+          in full control.
+        </p>
+      </div>
     </section>
   );
 }
